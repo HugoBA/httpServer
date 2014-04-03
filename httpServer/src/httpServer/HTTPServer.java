a78ad4f8cb4c792e4e197979faee11c06a18391a
QuentinFA <aoghar@gmail.com>
2014-04-03 12:10:34 +0200

    Almost final version

--- httpServer/src/httpServer/HTTPServer.java
+++ httpServer/src/httpServer/HTTPServer.java
@@ -12,7 +12,6 @@ import java.net.Socket;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
@@ -20,7 +19,6 @@ import java.util.StringTokenizer;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import polygon2DDetection.NotPolygonException;
 import polygon2DDetection.Polygon;
 import polygon2DDetection.PolygonDetection;
 import polygonDB.PatternFromDB;
@@ -55,7 +53,7 @@ public class HTTPServer extends Thread
 	/**
 	 * Approximation radius
 	 */
-	public final static double APROXIMATION = 0.01;
+	public final static double APROXIMATION = 0.5;
 
 	public HTTPServer(Socket client) 
 	{
@@ -128,9 +126,10 @@ public class HTTPServer extends Thread
 				}
 				JSONParser parser = new JSONParser();
 
-				System.out.println("Latitude : "+finaltab.get("latitude"));
-				System.out.println("Latitude : "+finaltab.get("longitude"));
+				System.out.println("Latitude : "+finaltab.get("latitude")
+						+" / Longitude : "+finaltab.get("longitude"));
 				
+				//Request to OverPass Turbo
 				InputStream inputStream = new URL("http://overpass-api.de/api/interpreter?data=%3Cosm-script%20output%3D%22json%22%3E%0A%20%20%3Cquery%20type%3D%22way%22%3E%0A%20%20%20%20%3"
 						+ "Caround%20lat%3D%22"+finaltab.get("latitude")+"%22%20lon%3D%22"+finaltab.get("longitude")+"%22%20radius%3D%22"+finaltab.get("radius")+"%22%2F%3E%0A"
 						+ "%20%20%20%20%3Chas-kv%20k%3D%22highway%22%20modv%3D%22%22%20v%3D%22%22%2F%3E%0A%20%20%20%20%3Chas-kv%20k%3D%22highway%22%20modv%3D%22not%22%20"
@@ -145,33 +144,34 @@ public class HTTPServer extends Thread
 				JSONArray jsonArray = (JSONArray) jsonObject.get("elements");
 				
 				Iterator<JSONObject> iterator = jsonArray.iterator();
-				// Liste de RoadNodePosition, pour créer les routes
+				// RoadNodePosition list, for Road creation
 				List<RoadNodePosition> nodeRoadList = new ArrayList<RoadNodePosition>();
-				//Liste de Position, représentant tout les noeuds
+				//Position list, representing all the nodes
 				ArrayList<Position> nodeList = new ArrayList<Position>();
-				//Liste des routes
+				//Road list
 				ArrayList<Road> roads = new ArrayList<Road>();
 				
 				while(iterator.hasNext())
 				{
 					JSONObject currentElement = iterator.next();
-					//Si c'est un noeud
+					//If that's a node
 					if (currentElement.get("type").equals("node"))
 					{
-						//On créé le RoadNodePosition correspondant, qu'on ajoute à la liste
+						//Creating the RoadNodePosition corresponding, which is added to the RoadNodePosition list
 						nodeRoadList.add(new RoadNodePosition(
 								Double.parseDouble(currentElement.get("id").toString()),
 								Double.parseDouble(currentElement.get("lat").toString()),
 								Double.parseDouble(currentElement.get("lon").toString()
 										)));
 					}
-					//Si c'est une route
+					//If that's a road
 					else if (currentElement.get("type").equals("way"))
 					{
-						//Liste temporaire pour les noeuds de la route
+						//Temp list for road node
 						ArrayList<Position> roadNodeList = new ArrayList<Position>();
 						JSONArray jsonWayNodes = (JSONArray) currentElement.get("nodes");
 						Iterator<Long> nodesIterator = jsonWayNodes.iterator();
+						//Browsing the JSON, seeking the node
 						while (nodesIterator.hasNext()) 
 						{
 							Double currentNode = Double.parseDouble(nodesIterator.next().toString());
@@ -187,121 +187,59 @@ public class HTTPServer extends Thread
 					}
 				}
 				
-				//A la fin, on convertit la liste de RoadNodePosition en liste de Position
+				System.out.println("Node list : Retreived from OverPass");
+				
+				//Converting the RoadNodePosition list to a Position list
 				for (int i = 0; i < nodeRoadList.size(); i++)
 					nodeList.add(nodeRoadList.get(i).toPosition());
 				
-				System.out.println("Node list :\n"+nodeList.toString());
-				System.out.println("Road list :\n"+roads.toString());
-				
-				System.out.println("Positions successfully added");
-				System.out.println("Starting seeking patterns");
-
-				
-				/*
-				 * The house :
-				 * 44.9146298, 4.9145186
-				 * 44.9148567, 4.9146612
-				 * 44.9149414, 4.9149929
-				 * 44.9147757, 4.9154621
-				 * 44.9145304, 4.9156112
-				 * 44.9143414, 4.915506
-				 */
-				
-				Position h1 = new Position(44.9146298, 4.9145186);
-				Position h2 = new Position(44.9148567, 4.9146612);
-				Position h3 = new Position(44.9149414, 4.9149929);
-				Position h4 = new Position(44.9147757, 4.9154621);
-				Position h5 = new Position(44.9145304, 4.9156112);
-				Position h6 = new Position(44.9143414, 4.915506);
-				ArrayList<Position> houseList = new ArrayList<Position>();
-				houseList.add(h1);
-				houseList.add(h2);
-				houseList.add(h3);
-				houseList.add(h4);
-				houseList.add(h5);
-				houseList.add(h6);
-				
-				/*Position t1 = new Position(0,0);
-				Position t2 = new Position(0,3);
-				Position t3 = new Position(3,1);*/
-				
-				Vector vh1 = new Vector(h1,h2);
-				Vector vh2 = new Vector(h2,h3);
-				Vector vh3 = new Vector(h3,h4);
-				Vector vh4 = new Vector(h4,h5);
-				Vector vh5 = new Vector(h5,h6);
-				Vector vh6 = new Vector(h6,h1);
-				/*Vector vt1 = new Vector(t1,t2);
-				Vector vt2 = new Vector(t2,t3);
-				Vector vt3 = new Vector(t3,t1);*/
-				
-				ArrayList<Vector> houseVectorList = new ArrayList<Vector>();
-				houseVectorList.add(vh1);
-				houseVectorList.add(vh2);
-				houseVectorList.add(vh3);
-				houseVectorList.add(vh4);
-				houseVectorList.add(vh5);
-				houseVectorList.add(vh6);
-				/*ArrayList<Vector> triangleVectorList = new ArrayList<Vector>();
-				triangleVectorList.add(vt1);
-				triangleVectorList.add(vt2);
-				triangleVectorList.add(vt3);
+				System.out.println("Road list : Successfully built from node list");
 				
 				
-				Polygon house = null, positionHouse = null;
-				Polygon triangle = null;
-				
-				house = new Polygon("The hard house", houseVectorList);
-				positionHouse = new Polygon(houseList, "The hard position house");
-				*/
+				//Getting the Polygon list from the XML file
 				ArrayList<Polygon> polygonList = PatternFromDB.getPolygonList();
-				//polygonList.add(house);
-				//polygonList.add(positionHouse);
-				System.out.println(polygonList.toString());
+				System.out.println("Starting seeking patterns : "+polygonList.size()+" pattern(s) to search...");
 				
 				JSONArray patternsList = new JSONArray();
 				JSONObject pattern = null;
 				JSONArray finalNodeList = null;
 				JSONObject node=null;
 				
-				ArrayList<ArrayList<ArrayList<Position>>> detectedPolygons = new ArrayList<ArrayList<ArrayList<Position>>>();
+				//List of detected polygons
+				ArrayList<ArrayList<ArrayList<Position>>> detectedPolygons = 
+						new ArrayList<ArrayList<ArrayList<Position>>>();
 				
+				//Getting the available Polygons
 				for(int i=0; i < polygonList.size(); i++)
 				{
 					detectedPolygons.add(PolygonDetection.isTherePolygon(nodeList, polygonList.get(i), APROXIMATION));
 				}
-				/*
-				ArrayList<ArrayList<Position>> houseResults = PolygonDetection.isTherePolygon(nodeList, 
-						house, APROXIMATION);
-				ArrayList<ArrayList<Position>> triangleResult = PolygonDetection.isTherePolygon(nodeList, 
-						triangle, APROXIMATION);
-				System.out.println("List of detected \"house\" :\n"+houseResults.toString());
-				System.out.println("List of detected \"triangle\" :\n"+triangleResult.toString());
-				System.out.println("Clean results :");
 				
-				ArrayList<ArrayList<Position>> cleanHouseResults = 
-						RoadCheckPolygon.getClearedListFromBadPolygonRoad(houseResults, roads);
-				ArrayList<ArrayList<Position>> cleanTriangleResults = 
-						RoadCheckPolygon.getClearedListFromBadPolygonRoad(triangleResult, roads);
-						*/
 				ArrayList<ArrayList<ArrayList<Position>>> cleanedPolygons = new ArrayList<ArrayList<ArrayList<Position>>>();
 				
+				//Cleaning the list from bad Polygons, which don't match with roads
 				for(int i=0; i < detectedPolygons.size(); i++)
 				{
-					cleanedPolygons.add(RoadCheckPolygon.getClearedListFromBadPolygonRoad(detectedPolygons.get(i), roads));
+					cleanedPolygons.add(RoadCheckPolygon.getClearedListFromBadPolygonRoad(
+							detectedPolygons.get(i), roads));
 				}
-				/*
-				System.out.println("Cleaned \"house\" list :\n"+cleanHouseResults.toString());
-				System.out.println("Cleaned \"triangle\" list :\n"+cleanTriangleResults.toString());
-				*/
 
-				System.out.println(cleanedPolygons.toString());
+				int nbValidePolygon = 0, nbFoundPolygon = 0;
+
+				for (int i = 0; i < detectedPolygons.size(); i++)
+					nbFoundPolygon += detectedPolygons.get(i).size();
 				
-				for (int i0 = 0; i0 < cleanedPolygons.size(); i0++)
+				for (int i = 0; i < cleanedPolygons.size(); i++)
+					nbValidePolygon += cleanedPolygons.get(i).size();
+
+				System.out.println(nbFoundPolygon+" Patterns found.");
+				System.out.println(nbValidePolygon+" Patterns left after the road check.");
+				
+				//Sending the cleaned list as a JSON object
+				for (int i0 = 0; i0 < detectedPolygons.size(); i0++)
 				{
 					ArrayList<ArrayList<Position>> currentCleanedPolygonList = cleanedPolygons.get(i0);
-					for(int i=0; i < currentCleanedPolygonList.size(); i++)
+					for(int i=0; i < 3 && i < currentCleanedPolygonList.size(); i++)
 					{
 						pattern=new JSONObject();
 						finalNodeList = new JSONArray();
@@ -317,9 +255,7 @@ public class HTTPServer extends Thread
 						patternsList.add(pattern);
 					}
 				}
-				
-				sendResponse(200, patternsList.toJSONString());
-				
+				sendResponse(200, patternsList.toJSONString());				
 			}
 		}
 		catch (Exception e)
--- httpServer/src/vector2D/VectorDetection.java
+++ httpServer/src/vector2D/VectorDetection.java
@@ -14,7 +14,7 @@ public abstract class VectorDetection
 	 * Detect Positions of the given collection which are on the vector applyVector line, applied on the Position startPosition
 	 * Return also Positions which are in the radius APROX from the line
 	 * Create a new PositionProportion for each Positions on the line, containing the corresponding proportion
-	 * 		applied on the vector to get the point (or its équivalent)
+	 * 		applied on the vector to get the point (or its equivalent)
 	 * @param v the vector to apply
 	 * @param a the position on which the vector will be applied
 	 * @param pos the collection of Position for searching
